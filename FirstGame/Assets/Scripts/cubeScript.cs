4ca4f17c882b69413801443bf3a8a441204f1793
luigi seminara <seminara.luigi@gmail.com>
2019-07-18 16:57:30 +0200

    Pronto

---
+++ FirstGame/Assets/Scripts/cubeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour
+{
+    public GameObject Cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(Cube, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)), new Quaternion(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)));
+        }
+    }
+}
